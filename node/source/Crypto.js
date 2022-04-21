26de79c18125d3752ad184065b0de2241f7e4c80
terrablue <102580937+terrablue@users.noreply.github.com>
2022-04-21 17:11:26 +0200

    remove Crypto.hash

--- node/source/Crypto.js
+++ node/source/Crypto.js
@@ -1,12 +1,6 @@
-import {createHash, randomBytes} from "crypto";
-
-const encoding = "utf8";
+import {randomBytes} from "crypto";
 
 export default class {
-  static hash(data, algorithm = "sha256", digest = "base64") {
-    return createHash(algorithm).update(data, encoding).digest(digest);
-  }
-
   static random(length) {
     return randomBytes(length);
   }
