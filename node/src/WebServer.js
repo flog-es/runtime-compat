ecf547a10f478f1bcd4602f74f2bae1a8bd4111f
terrablue <102580937+terrablue@users.noreply.github.com>
2022-07-19 00:13:24 +0200

    [web] revert WebServer to node style

--- node/src/WebServer.js
+++ node/src/WebServer.js
@@ -26,35 +26,9 @@ const RequestWrapper = class {
   }
 };
 
-const ResponseWrapper = class {
-  constructor(response) {
-    this.response = response;
-    this.status = 200;
-  }
-
-  setHeader(key, value) {
-    this.response.setHeader(key, value);
-  }
-
-  setStatus(status) {
-    this.status = status;
-  }
-
-  setBody(body) {
-    this.body = body;
-  }
-
-  end() {
-    this.response.writeHead(this.status);
-    this.response.end(this.body);
-  }
-};
-
 export default class {
   constructor(conf, handler) {
-    this.handler = (request, response) =>
-      handler(new RequestWrapper(request), new ResponseWrapper(response));
-    this.server = createServer(conf, this.handler);
+    this.server = createServer(conf, handler);
   }
 
   listen(port, host) {
